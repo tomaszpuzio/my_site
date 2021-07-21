c7efd4627f021f565fc5147ca70a43e13b7a21c5
tomasz <tomekpdevelopment@gmail.com>
2021-07-21 08:30:23 +0200

    Set up initial views and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def starting_page(request):
+    return HttpResponse("I am here")
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
