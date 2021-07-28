f1631655bb94186d7de1af25c61a88b46f9078e2
tomasz <tomekpdevelopment@gmail.com>
2021-07-28 04:51:21 +0200

    add more posts and create dynamic content

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,67 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import date
 
+from django.shortcuts import render
+
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Tomasz",
+        "date": date(2021,7,21),
+        "title": "Mountain Hiking",
+        "excerpt": "Beautiful vies in the mountains",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Possimus, eum magnam! Ipsum voluptate ad libero, quam tempora rem? 
+            Error, explicabo aliquam deserunt quis nihil iusto reiciendis nemo 
+            ratione suscipit corrupti.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Possimus, eum magnam! Ipsum voluptate ad libero, quam tempora rem? 
+            Error, explicabo aliquam deserunt quis nihil iusto reiciendis nemo 
+            ratione suscipit corrupti.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Possimus, eum magnam! Ipsum voluptate ad libero, quam tempora rem? 
+            Error, explicabo aliquam deserunt quis nihil iusto reiciendis nemo 
+            ratione suscipit corrupti.
+        """
+    },
+    {
+        "slug": "funny",
+        "image": "coding.jpg",
+        "author": "Tomasz",
+        "date": date(2021,7,15),
+        "title": "Funny things about coding",
+        "excerpt": "Coding is great",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Possimus, eum magnam! Ipsum voluptate ad libero, quam tempora rem? 
+            Error, explicabo aliquam deserunt quis nihil iusto reiciendis nemo 
+            ratione suscipit corrupti.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Possimus, eum magnam! Ipsum voluptate ad libero, quam tempora rem? 
+            Error, explicabo aliquam deserunt quis nihil iusto reiciendis nemo 
+            ratione suscipit corrupti.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Possimus, eum magnam! Ipsum voluptate ad libero, quam tempora rem? 
+            Error, explicabo aliquam deserunt quis nihil iusto reiciendis nemo 
+            ratione suscipit corrupti.
+        """
+    }
+]
+
+def get_date(post):
+    return post["date"]
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
     return render(request, "blog/all-posts.html")
