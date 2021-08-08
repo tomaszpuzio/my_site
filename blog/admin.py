f79d662a99024c7a70e25a811255809f38eeb61d
tomasz <tomekpdevelopment@gmail.com>
2021-08-08 12:32:30 +0200

    set up admin panel and models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Author, Tag, Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "author", "excerpt", "date")
+
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.db.models.fields import CharField, DateField, EmailField, TextField, SlugField
+from django.db.models.fields.related import ForeignKey, ManyToManyField
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = CharField(max_length=100)
+    last_name = CharField(max_length=100)
+    email_address = EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Tag(models.Model):
+    caption = CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+
+class Post(models.Model):
+    author = ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    title = CharField(max_length=150)
+    excerpt = CharField(max_length=200)
+    image_name = CharField(max_length=100)
+    date = DateField(auto_now=True)
+    slug = SlugField(unique=True, db_index=True)
+    tags = ManyToManyField(Tag)
+    content = TextField(validators=[MinLengthValidator(10)])
+
