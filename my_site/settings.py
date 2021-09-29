c6ebf4ac4c2a4c7c79f5eee7dc590580cfd8b6e9
tomasz <tomekpdevelopment@gmail.com>
2021-09-29 19:16:57 +0200

    set up the code to deploy it with Postgres and static files storage

--- my_site/settings.py
+++ my_site/settings.py
@@ -21,15 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SECRET_KEY")
+SECRET_KEY = getenv("SECRET_KEY","${env:SECRET_KEY}")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("IS_DEVELOPMENT", True)
 
 ALLOWED_HOSTS = [
-    getenv("APP_HOST")
+    getenv("APP_HOST", "127.0.0.1"),
 ]
-
 
 # Application definition
 
@@ -81,8 +80,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'type_user_name',
+        'PASSWORD': 'type_password',
+        'HOST': 'django-blog.cocf4lwqjq4b.eu-west-3.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
